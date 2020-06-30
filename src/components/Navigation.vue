235ff0ead81a53ed8951cca8321e4e3580c8f29a
Krishna Jha <krishnajha366@gmail.com>
2020-06-30 21:37:13 +0530

    Few changes in navigation and files

---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navigation">
+        <h1>News App</h1>
+        <div class="center">
+            This is a simple News App made using <strong class="red--text text--lighten-1">Vue.js</strong> and <strong class="red--text text--lighten-1">Vuetify</strong>.
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+    .navigation{
+        display: flex;
+        flex-direction: column;;
+        justify-content: center;
+        align-items: center;
+        min-height: 80px;
+    }
+
+    .center{
+        text-align: center;
+    }
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="carousel-container">
+    <v-carousel
+    cycle
+    >
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
 }
 </script>
